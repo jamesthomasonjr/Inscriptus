7240a9e405834088a47d412734748a4df091f3a8
James Thomason <jamesthomasonjr@Jamess-MacBook-Pro.local>
2014-07-25 21:25:17 -0400

    Modified XmlStrategy code to appease scrutinizer-ci

--- src/core/models/hypermediaserializer/strategies/XmlStrategy.php
+++ src/core/models/hypermediaserializer/strategies/XmlStrategy.php
@@ -8,13 +8,20 @@ class XmlStrategy
     public function serialize($hypermedia)
     {
         $output = new \SimpleXmlElement('<document></document>');
-        $output->title = $hypermedia->getTitle();
-        $output->href = $hypermedia->getHref();
+        $output->addChild('title', $hypermedia->getTitle());
+        $output->addChild('href', $hypermedia->getHref());
+
+        if(count($hypermedia->getRels())) {
+            $relsNode = $output->addChild('rels');
+        }
 
         foreach($hypermedia->getRels() as $rel) {
             $title = $rel->getTitle();
-            $output->rels->$title->href = $rel->getHref();
-            $output->rels->$title->title = $rel->getTitle();
+
+            $relNode = $relsNode->addChild($title);
+
+            $relNode->addChild('href', $rel->getHref());
+            $relNode->addChild('title', $rel->getTitle());
         }
 
         return $output->asXML();
